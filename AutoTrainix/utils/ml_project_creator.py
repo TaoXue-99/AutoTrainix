e55e3bf3685c9d545c712556b65e90b92882e8ad
xuetao <xuetao_99@163.com>
2025-06-22 14:47:30 +0800

    Add evaluation_results to data subfolders
    Included 'evaluation_results' as a subdirectory under the 'data' folder in the default project structure to better organize evaluation outputs.

--- AutoTrainix/utils/ml_project_creator.py
+++ AutoTrainix/utils/ml_project_creator.py
@@ -17,7 +17,7 @@ class MLProjectCreator:
         """
         # Default subfolder structure (with subdirectories)
         default_structure: Dict[str, List[str]] = {
-            "data": ["raw", "output", "processed","mappings"],
+            "data": ["raw", "output", "processed","mappings","evaluation_results"],
             "models": ["checkpoints"],
             "utils": [],
             "logs": [],
